701f9fe80c4589b7856dd1d2501d81c05359d99a
asknishant <asknishant.39@gmail.com>
2020-11-09 18:28:39 +0530

    First version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## [1.0.6] - TODO: Add release date.
+## [1.1.0] - TODO: Add release date.
 
 * This is the first release
--- lib/flipping_button.dart
+++ lib/flipping_button.dart
@@ -1,7 +1,3 @@
 library flipping_button;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/flip_button.dart';
